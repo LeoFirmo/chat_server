60aa2784656a54fb3730f37d716e53bf6c3857e9
LeoFirmo <139890353+LeoFirmo@users.noreply.github.com>
2025-01-12 23:00:35 -0300

    Update server2.js

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.use(express.static("public"));
 
 app.get("/", (req, res) => {
     console.log("Cliente acessou a página principal.");
-    res.sendFile(__dirname + "chat.html");
+    res.sendFile("chat.html");
 });
 
 io.on("connection", (socket) => {
